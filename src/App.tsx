fd7c0f84003c8526e2b1533d75af2d90e2b8063f
Jedrzej Wiewiora <jedrzej.wiewiora@gmail.com>
2025-09-14 20:08:42 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { riddles as allRiddles } from "./riddles";
+import { twMerge } from "tailwind-merge";
 
 function App() {
   const [riddleCategory, setRiddleCategory] = useState<
@@ -12,7 +13,7 @@ function App() {
 
   if (!riddleCategory || !riddles) {
     return (
-      <div className="min-h-screen w-full flex flex-col items-center justify-center text-white text-center px-2 bg-gradient-to-br from-indigo-700 via-purple-500 to-pink-400">
+      <Wrapper>
         <div className="text-xl sm:text-3xl md:text-4xl w-full max-w-md mx-auto">
           <div className="text-4xl sm:text-7xl md:text-8xl font-extrabold break-words drop-shadow-lg font-[Comic Sans MS,Comic Sans,cursive] animate-bounce text-yellow-300">
             GRA W ZAGADKI
@@ -32,7 +33,7 @@ function App() {
             ))}
           </div>
         </div>
-      </div>
+      </Wrapper>
     );
   }
 
@@ -40,9 +41,11 @@ function App() {
 
   if (!riddle) {
     return (
-      <div className="min-h-screen w-full flex items-center justify-center text-white px-2 bg-gradient-to-br from-indigo-700 via-purple-500 to-pink-400">
+      <Wrapper>
         <div className="flex flex-col items-center justify-center w-full h-full max-w-2xl mx-auto">
-          <div className="text-4xl sm:text-7xl md:text-8xl font-extrabold mb-8 text-center drop-shadow-lg font-[Comic Sans MS,Comic Sans,cursive] text-yellow-300 animate-bounce">KONIEC</div>
+          <div className="text-4xl sm:text-7xl md:text-8xl font-extrabold mb-8 text-center drop-shadow-lg font-[Comic Sans MS,Comic Sans,cursive] text-yellow-300 animate-bounce">
+            KONIEC
+          </div>
           <div className="w-full max-w-xl mx-auto bg-gray-100 flex items-center justify-center text-black font-bold text-lg sm:text-2xl md:text-3xl py-16 px-4 mb-8 text-center rounded-3xl shadow-2xl border-4 border-pink-200">
             Twój wynik to {goodCount} / {riddles.length}
           </div>
@@ -57,16 +60,20 @@ function App() {
             Zagraj ponownie
           </button>
         </div>
-      </div>
+      </Wrapper>
     );
   }
 
   return (
-    <div className="min-h-screen w-full flex items-center justify-center text-white text-center px-2 bg-gradient-to-br from-indigo-700 via-purple-500 to-pink-400">
+    <Wrapper>
       <div className="flex flex-col items-center w-full">
         <div className="text-xl sm:text-3xl md:text-4xl mb-2 font-semibold text-pink-200">
-          <div className="text-lg sm:text-2xl md:text-4xl font-bold text-yellow-300 drop-shadow-lg">ZAGADKA {riddleNumber + 1} / {riddles.length}</div>
-          <div className="text-base sm:text-lg md:text-2xl mt-2 font-semibold text-pink-200">Poprawne odpowiedzi: {goodCount} / {riddleNumber}</div>
+          <div className="text-lg sm:text-2xl md:text-4xl font-bold text-yellow-300 drop-shadow-lg">
+            ZAGADKA {riddleNumber + 1} / {riddles.length}
+          </div>
+          <div className="text-base sm:text-lg md:text-2xl mt-2 font-semibold text-pink-200">
+            Poprawne odpowiedzi: {goodCount} / {riddleNumber}
+          </div>
         </div>
         <div className="mt-4 sm:mt-8 w-full max-w-xl mx-auto bg-yellow-100 flex items-center justify-center text-gray-800 text-2xl sm:text-3xl md:text-4xl font-extrabold break-words py-10 px-4 rounded-3xl shadow-2xl border-4 border-pink-200">
           <div>{riddle?.question}</div>
@@ -85,7 +92,11 @@ function App() {
           ))}
         </div>
         <div
-          className={`fixed left-0 top-0 w-full h-full flex flex-col items-center justify-center transition-opacity z-50 ${showCorrect === true ? "bg-gradient-to-br from-green-400 via-yellow-300 to-pink-300" : ""}`}
+          className={`fixed left-0 top-0 w-full h-full flex flex-col items-center justify-center transition-opacity z-50 ${
+            showCorrect === true
+              ? "bg-gradient-to-br from-green-400 via-yellow-300 to-pink-300"
+              : ""
+          }`}
           style={{
             opacity: showCorrect === true ? 1 : 0,
             pointerEvents: showCorrect === true ? "auto" : "none",
@@ -107,7 +118,11 @@ function App() {
         </div>
 
         <div
-          className={`fixed left-0 top-0 w-full h-full flex flex-col items-center justify-center transition-opacity z-50 ${showCorrect === false ? "bg-gradient-to-br from-red-400 via-pink-300 to-yellow-200" : ""}`}
+          className={`fixed left-0 top-0 w-full h-full flex flex-col items-center justify-center transition-opacity z-50 ${
+            showCorrect === false
+              ? "bg-gradient-to-br from-red-400 via-pink-300 to-yellow-200"
+              : ""
+          }`}
           style={{
             opacity: showCorrect === false ? 1 : 0,
             pointerEvents: showCorrect === false ? "auto" : "none",
@@ -127,8 +142,20 @@ function App() {
           </button>
         </div>
       </div>
-    </div>
+    </Wrapper>
   );
 }
+
+const Wrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return (
+    <div
+      className={twMerge(
+        "min-h-screen w-full flex flex-col items-center justify-center text-white text-center px-2 bg-gradient-to-br from-indigo-700 via-purple-500 to-pink-400"
+      )}
+    >
+      {children}
+    </div>
+  );
+};
 
 export default App;
